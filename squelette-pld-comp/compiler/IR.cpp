3cc38b7e62318330f9cee9ce61b0d55223b726f3
Elie-Hasbani <elie.hasbani498@gmail.com>
2026-03-06 10:20:19 +0100

    added some functions

--- squelette-pld-comp/compiler/IR.cpp
+++ squelette-pld-comp/compiler/IR.cpp
@@ -4,3 +4,26 @@
 IRInstr::IRInstr(BasicBlock *bb_, Operation op, Type t, vector<string> params) : bb(bb_), op(op), t(t), params(params)
 {
 }
+
+BasicBlock::BasicBlock(CFG *cfg, string entry_label) : cfg(cfg), label(entry_label), exit_true(nullptr), exit_false(nullptr)
+{
+}
+
+void BasicBlock::add_IRInstr(IRInstr::Operation op, Type t, vector<string> params)
+{
+    instrs.push_back(new IRInstr(this, op, t, params));
+}
+
+CFG::CFG(DefFonction *ast) : ast(ast), nextFreeSymbolIndex(0), nextBBnumber(0), current_bb(nullptr)
+{
+}
+
+string CFG::new_BB_name()
+{
+    return ast->getName() + "_BB" + to_string(nextBBnumber++);
+}
+
+void CFG::add_bb(BasicBlock *bb)
+{
+    bbs.push_back(bb);
+}
