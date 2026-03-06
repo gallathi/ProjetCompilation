a15418f9a8e3225e85c65dae565c5954bcb19ab9
ewybouw <esteban.wybouw@gmail.com>
2026-03-06 10:46:46 +0100

    CFG

--- squelette-pld-comp/compiler/IR.cpp
+++ squelette-pld-comp/compiler/IR.cpp
@@ -1,6 +1,8 @@
 #include "IR.h"
 #include <iostream>
 
+using namespace std;
+
 void CFG::gen_asm_prologue(ostream& o) {
 	#ifdef __APPLE__
     o << ".globl _main\n" ;
@@ -10,17 +12,57 @@ void CFG::gen_asm_prologue(ostream& o) {
     o << " main: \n" ;
     #endif
 
-	std::cout<< "    pushq %rbp\n";
-	std::cout<< "    movq %rsp, %rbp\n";
+	o << "    pushq %rbp\n";
+	o << "    movq %rsp, %rbp\n";
 }
 
 void CFG::gen_asm_epilogue(ostream& o) {
-	std::cout << "    popq %rbp\n";
-    std::cout << "    ret\n";
+	o << "    popq %rbp\n";
+    o << "    ret\n";
 
     #ifndef __APPLE__
-        std::cout << ".section .note.GNU-stack,\"\",@progbits\n";
+        o << ".section .note.GNU-stack,\"\",@progbits\n";
     #endif
+}
+
+void CFG::gen_asm(ostream &o) {
+	current_bb = bbs[0];
+	current_bb->gen_asm(o);
+	while (current_bb->exit_true != nullptr) {
+		current_bb = current_bb->exit_true;
+	}
+}
+
+string CFG::IR_reg_to_asm(string reg) {
+	return "-" + itos(SymbolIndex[reg]) + "%(rbp)";
+}
+
+void CFG::add_to_symbol_table(string name, Type t) {
+	SymbolType[name] = t;
+	SymbolIndex[name] = nextFreeSymbolIndex;
+	nextFreeSymbolIndex++;
+}
+
+string CFG::create_new_tempvar(Type t) {
+	string newName = "!tmp" + itos(nextFreeSymbolIndex);
+	return newName;
+}
+
+int CFG::get_var_index(string name) {
+	return SymbolIndex[name];
+}
+
+Type CFG::get_var_type(string name) {
+	return SymbolType[name];
+}
+
+string CFG::new_BB_name() {
+	string newName = "bb" + itos(nextBBnumber);
+	return newName; 
+}
+
+void CFG::add_bb(BasicBlock *bb) {
+	bbs.push_back(bb);
 }
 
 IRInstr::IRInstr(BasicBlock *bb_, Operation op, Type t, vector<string> params) : bb(bb_), op(op), t(t), params(params)
