fea1eff76860847072a6aa405cb1077b49bb5ffe
gallathi <78640476+gallathi@users.noreply.github.com>
2026-03-12 14:30:16 +0100

    Delete squelette-pld-comp/ifcc-test-output directory

--- squelette-pld-comp/ifcc-test-output/testfiles-101_constchar/input.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char c = 'abcdefghijklmnopqrstuvwxyz';
-    printf("Character: %c\n", c);
-    return 0;
-}
-
--- squelette-pld-comp/ifcc-test-output/testfiles-5_3varsEtAffectations/input.c
+++
@@ -1,8 +0,0 @@
-int main() {
-    int a, b, c;
-    a = 1;
-    c = 4;
-    b = a;
-    c = b;
-    return c;
-}
